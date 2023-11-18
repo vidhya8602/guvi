f617ad2bdf6aa43408c552c9324cef1b3179a698
vidhya8602 <146460105+vidhya8602@users.noreply.github.com>
2023-11-18 14:14:26 +0530

    Add files via upload

--- ttoima.py
+++ ttoima.py
@@ -1,1 +1,20 @@
+import requests
+import streamlit as st
 
+API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {"Authorization": "Bearer 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"}
+st.title("image generation application")
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.content
+prompt=st.text_input("enter ur statement")
+bt=st.button("enter")
+if bt:
+    image_bytes = query({
+        "inputs": prompt,
+    })
+    # You can access the image with PIL.Image for example
+    import io
+    from PIL import Image
+    images = Image.open(io.BytesIO(image_bytes))
+    st.image(images)
